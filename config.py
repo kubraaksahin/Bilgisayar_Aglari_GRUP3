c59b8529672167499809007ae9e0da54f4839fbc
omerarsak <161201273+omerarsak@users.noreply.github.com>
2025-12-17 00:56:05 +0300

    Update comments and add ACO hyperparameters

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 # config.py
 
 # -----------------------------
-# 2) Ortak ağırlık seti (8 tane farklı ağırlık senaryosundan Tüm algoritmalar için seçilen Ağırlıklar)
+# 1) Ortak ağırlık seti (8 tane farklı ağırlık senaryosundan Tüm algoritmalar için seçilen Ağırlıklar)
 # -----------------------------
 W_DELAY = 0.04
 W_RELIABILITY = 0.95
 W_RESOURCE = 0.01
+
+# -----------------------------
+# 2) ACO Hiperparametre seçimi
+# -----------------------------
+ACO_N_ANTS = 80       
+ACO_N_ITERATIONS = 80
