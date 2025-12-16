9167afd31e08807d417c2e0ad14160da1c2895fd
omerarsak <161201273+omerarsak@users.noreply.github.com>
2025-12-17 00:38:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,3 @@
 W_DELAY = 0.04
 W_RELIABILITY = 0.95
 W_RESOURCE = 0.01
-
-
-# -----------------------------
-# 3) ACO parametreleri (Final için seçilen)
-# -----------------------------
-ACO_N_ANTS = 80        # örnek
-ACO_N_ITERATIONS = 80  # örnek
-
