de5e62ed9b81a6c62b12256f71dc67a6ec19b49c
omerarsak <161201273+omerarsak@users.noreply.github.com>
2025-12-17 00:37:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# -----------------------------
+# 2) Ortak ağırlık seti (8 tane farklı ağırlık senaryosundan Tüm algoritmalar için seçilen Ağırlıklar)
+# -----------------------------
+W_DELAY = 0.04
+W_RELIABILITY = 0.95
+W_RESOURCE = 0.01
+
+
+# -----------------------------
+# 3) ACO parametreleri (Final için seçilen)
+# -----------------------------
+ACO_N_ANTS = 80        # örnek
+ACO_N_ITERATIONS = 80  # örnek
+
